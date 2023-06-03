7c2880c0b81b1b70a815e11d8ef3bf80febd078b
Edvinas <edvingvozdovic@gmail.com>
2023-06-03 12:39:47 +0300

    nd-iki8

---
+++ Mechanika/black.php
@@ -0,0 +1,24 @@
+<?php 
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+   
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VELNIO KODAI</title>
+</head>
+<body style="background-color:black";>
+<form action="http://localhost/egprojektas/Mechanika/black.php" method="post">
+<button type="submit"> </button>
+<input type="checkbox" name="" id="">
+</form>
+</body>
---
+++ Mechanika/pink.php
@@ -0,0 +1,29 @@
+<?php 
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VELNIO KODAI</title>
+</head>
+<body style="background-color:pink";>
+
+
+
+
+ 
+    
+
+      <form action="http://localhost/egprojektas/Mechanika/rose.php" method="post">
+       <button type="submit">GO to ROSE</button>
+       </form>
+
+</body>
